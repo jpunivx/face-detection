2bed2ddc6849d5a8605ff0555e4eab0cb621aafb
jpildush <jpildush@ryerson.ca>
2021-12-14 20:55:06 -0500

    added dlib and opencv + pycharm

---
+++ py-env/py-code/main.py
@@ -0,0 +1,34 @@
+
+__OPTION_ONE__ = '1'
+__OPTION_TWO__ = '2'
+__OPTION_THREE__ = '3'
+__OPTION_FOUR__ = '4'
+
+def start(name):
+    while True:
+        menuOption = ''
+        print("|===========================================|")
+        print("| 1 - Viola–Jones Object Detection          |")
+        print("| 2 - Histogram of the Oriented Gradients   |")
+        print("| 3 - Convolutional Neural Networks (CNN)   |")
+        print("| 4 -                                       |")
+        print("|                                           |")
+        print("|>")
+        input(menuOption)
+
+        if menuOption == __OPTION_ONE__:
+            pass
+        elif menuOption == __OPTION_TWO__:
+            pass
+        elif menuOption == __OPTION_THREE__:
+            pass
+        elif menuOption == __OPTION_FOUR__:
+            break
+
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    # Launch Options Menu
+    start()
