7130265cbdcafdb021299e5890da509e3493373e
jpildush <jpildush@ryerson.ca>
2021-12-18 19:19:15 -0500

    fixed running script and changed gitignore

--- run_face_detection.sh
+++ run_face_detection.sh
@@ -6,10 +6,14 @@ cd ./py-env
 
 source ./bin/activate
 
+pip install -r requirements_base.txt
+
 pip install -r requirements.txt
+
+cd ..
 
 current_directory=$(pwd)
 
-python3 ./py-code/main.py $current_directory
+python3 ./py-env/py-code/main.py $current_directory
 
 deactivate
